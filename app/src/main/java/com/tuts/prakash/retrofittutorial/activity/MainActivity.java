a36748af3fd103f9d20618f2452e8cb0ce599ad8
jksttymm <rdxsttymm@gmail.com>
2020-07-15 08:19:26 +0530

    Migrated to android x

--- app/src/main/java/com/tuts/prakash/retrofittutorial/activity/MainActivity.java
+++ app/src/main/java/com/tuts/prakash/retrofittutorial/activity/MainActivity.java
@@ -1,10 +1,10 @@
 package com.tuts.prakash.retrofittutorial.activity;
 
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.widget.Toast;
 
 import com.tuts.prakash.retrofittutorial.R;
